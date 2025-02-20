3391af753c31cca49a6e837a06b890b47f2ce204
Rohan <vivekjariwala1319@gmail.com>
2025-02-20 10:48:23 +0530

    done

--- app/src/main/java/com/example/bookapp/Books.java
+++ app/src/main/java/com/example/bookapp/Books.java
@@ -2,50 +2,112 @@ package com.example.bookapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Books extends AppCompatActivity {
 
-    private ImageView back, share, download;
+    private LinearLayout img1, img2, img3, img4;
+    private LinearLayout day1, day2, day3, day4;
+    private LinearLayout pic1, pic2, pic3, pic4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_main);
         initBinding();
 
-        back.setOnClickListener(v -> {
+        img1.setOnClickListener(v -> {
             Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.img1);
             startActivity(intent);
         });
 
-        ImageView image = findViewById(R.id.image);
+        img2.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.one);
+            startActivity(intent);
+        });
 
-        Intent intent = getIntent();
-        int i = intent.getIntExtra("image",0);
+        img3.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.img3);
+            startActivity(intent);
+        });
 
-        if (i != 0){
-            image.setImageResource(i);
-        }else {
-            image.setImageResource(R.drawable.img);
-        }
+        img4.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.img4);
+            startActivity(intent);
+        });
 
+        day1.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.day1);
+            startActivity(intent);
+        });
+
+        day2.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.day4);
+            startActivity(intent);
+        });
+
+        day3.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.day3);
+            startActivity(intent);
+        });
+
+        day4.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.day5);
+            startActivity(intent);
+        });
+
+        pic1.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.pic1);
+            startActivity(intent);
+        });
+
+        pic2.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.pic2);
+            startActivity(intent);
+        });
+
+        pic3.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.pic3);
+            startActivity(intent);
+        });
+
+        pic4.setOnClickListener(v -> {
+            Intent intent = new Intent(Books.this, HomeScreen.class);
+            intent.putExtra("image",R.drawable.pic4);
+            startActivity(intent);
+        });
     }
 
     public void initBinding(){
 
-        back = findViewById(R.id.back);
-        share = findViewById(R.id.share);
-        download = findViewById(R.id.download);
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.one);
+        img3 = findViewById(R.id.img3);
+        img4 = findViewById(R.id.img4);
 
+        day1 = findViewById(R.id.day1);
+        day2 = findViewById(R.id.day2);
+        day3 = findViewById(R.id.day3);
+        day4 = findViewById(R.id.day4);
+
+        pic1 = findViewById(R.id.pic1);
+        pic2 = findViewById(R.id.pic2);
+        pic3 = findViewById(R.id.pic3);
+        pic4 = findViewById(R.id.pic4);
     }
 }
--- app/src/main/java/com/example/bookapp/HomeScreen.java
+++ app/src/main/java/com/example/bookapp/HomeScreen.java
@@ -1,6 +1,10 @@
 package com.example.bookapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,16 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView image = findViewById(R.id.imageView2);
+
+        Intent intent = getIntent();
+        int i = intent.getIntExtra("image",0);
+
+        if (i != 0){
+            image.setImageResource(i);
+        }else {
+            image.setImageResource(R.drawable.one);
+        }
     }
 }
